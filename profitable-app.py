4311b10d1403ba738ff5173ea0da273b649185a2
esraakblk <esraakbulak@gmail.com>
2024-10-09 20:51:09 +0300

    Deleted wrong data and removed duplicate data

--- profitable-app.py
+++ profitable-app.py
@@ -37,3 +37,39 @@ def explore_data(dataset, start, end, rows_and_columns=False):
 print(android_header)
 print("\n")
 explore_data(android, 0, 3, True)
+
+#The Google Play data set has a dedicated discussion section, and we can see that one of 
+#the discussions outlines an error for row 10472. Let's print this row and compare it against 
+#the header and another row that is correct.
+
+print(android[10472], "\n") #incorrect row
+print(android_header, "\n") #header
+print(android[0]) #correct row
+
+print(len(android)) 
+del(android[10472])
+print(len(android))
+
+#Removing Duplicate Entries
+#Part One
+#If we explore the Google Play data set long enough, we'll find that some apps have more than one entry. 
+#For instance, the application Instagram has four entries:
+
+for app in android:
+    name = app[0]
+    app_names = ["Instagram", "Facebook",]
+    if name == app_names:
+        print(app)
+
+duplicated_app = []
+uniq_app = set()
+for app in android:
+    name = app[0]
+    if name in uniq_app:
+        duplicated_app.append(name)
+    else:
+        uniq_app.add(name)
+
+print('Number of duplicate apps:', len(duplicated_app))
+print('\n')
+print('Examples of duplicate apps:', duplicated_app[:15])
